d4d1ad990729a8f3566b8faad25a7828fa53d1c9
somya <somya.agarwal@globant.com>
2022-03-03 12:45:02 +0530

    file modified

--- modules/S3/bucket.tf
+++ modules/S3/bucket.tf
@@ -9,13 +9,13 @@ resource "aws_s3_bucket" "terraform-state" {
       enabled = false
     }
 
-    server_side_encryption_configuration {
-      rule {
-          apply_server_side_encryption_by_default {
-              sse_algorithm = "AES256"
-          }
-      }
-    }
+    # server_side_encryption_configuration {
+    #   rule {
+    #       apply_server_side_encryption_by_default {
+    #           sse_algorithm = "AES256"
+    #       }
+    #   }
+    # }
   
 }
 
