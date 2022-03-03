7cae1cd52ec15c752b40025d5dbc658389a18814
somya <somya.agarwal@globant.com>
2022-03-03 13:04:02 +0530

    file modified

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "state-file-bucket-01"
+    key    = "myterraformstatefile"
+    region = "eu-west-1"
+  }
+}
+
+
 provider "aws" {
     region = var.AWS_Region
 
